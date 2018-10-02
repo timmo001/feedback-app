8209e82fe8cf295ee1e0dacca394de8e5bce9758
Timmo <contact@timmo.xyz>
2018-10-02 10:06:23 +0100

    :hammer: theming

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
 import blueGrey from '@material-ui/core/colors/blueGrey';
-import green from '@material-ui/core/colors/blueGrey';
 import red from '@material-ui/core/colors/red';
-import grey from '@material-ui/core/colors/grey';
 import 'typeface-roboto';
 import '@mdi/font/css/materialdesignicons.min.css';
 import './App.css';
@@ -12,19 +11,7 @@ import Root from './Components/Root';
 const theme = createMuiTheme({
   palette: {
     primary: green,
-    secondary: grey,
-    mainBackground: grey[100],
-    backgrounds: {
-      light: blueGrey[600],
-      main: blueGrey[400],
-      dark: blueGrey[200],
-      disabled: grey[200],
-    },
-    defaultText: {
-      light: grey[700],
-      main: grey[800],
-      dark: grey[900],
-    },
+    secondary: blueGrey,
     error: red,
     contrastThreshold: 3,
     tonalOffset: 0.2,
